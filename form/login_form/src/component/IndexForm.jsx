b73d754685b9dc71831e4c48b226876735268ca4
KowshikP07 <kowshikp.23cse@kongu.edu>
2025-11-17 12:36:59 +0530

    form is being decorated

--- form/login_form/src/component/IndexForm.jsx
+++ form/login_form/src/component/IndexForm.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 import '../css/Form.css'
 const IndexForm = () => {
-    const handlesubmit=()=>{
+    const handlesubmit = () => {
         console.log("submitted")
     }
-  return (
-    <div className="formdiv">
-    <form onSubmit={handlesubmit} className="main_form">
-        <input type="text"/>
-        <input type="email" name="email" id="email"/>
-        <input type="number" name="num" id="num" />
-        <input type="password" name="pass" id="pass" />
-        <button>Submit</button>        
-    </form>
-    </div>
-  )
+    return (
+        <div className="formdiv">
+            <form onSubmit={handlesubmit} className="main_form">
+                <div className="name in">
+                    <span className='title'>Username</span>
+                    <input type="text" />
+                    <span className='error'></span>
+                </div>
+                <div className="email in">
+                    <span className='title'>email</span>
+                    <input type="email" name="email" id="email" />
+                    <span className='error'></span>
+                </div>
+                <div className="password in">
+                    <span className='title'>Password</span>
+                    <input type="password" name="pass" id="pass" />
+                    <span className='error'></span>
+                </div>
+                <div className="confirmpass in">
+                    <span className='title'>Confirm Password</span>
+                    <input type="password" name="pass" id="pass" />
+                    <span className='error'></span>
+                </div>
+                <button className='submit'>Submit</button>
+            </form>
+        </div>
+    )
 }
 
 export default IndexForm
